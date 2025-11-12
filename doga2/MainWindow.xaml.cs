218b55d8a3fa2d0d730b62e4575fa0a615c4c23b
Kovács Zsombor <125221225+KoZsombat@users.noreply.github.com>
2025-11-12 08:56:25 +0100

    Add project files.

---
+++ doga2/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace doga2;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public int allQuantity = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void add_Click(object sender, RoutedEventArgs e)
+    {
+        if (name.Text != "" || price.Text != "" || quantity.Text != "")
+        {
+            if (double.TryParse(price.Text, out var priceOfItem) && int.TryParse(quantity.Text, out var quantityOfItem))
+            { 
+                list.Items.Add($"{name?.Text} - {priceOfItem} Ft - {quantityOfItem} db");
+                allQuantity += quantityOfItem;
+                stats.Content = $"Összes termék darabszáma: {allQuantity} db";
+            } else
+            {
+                MessageBox.Show("Hibás adatbevitel!");
+            }
+        } else
+        {
+            MessageBox.Show("Tölts ki minden mezőt!");
+        }
+    }
+
+    private void delete_Click(object sender, RoutedEventArgs e)
+    {
+        var selectedItem = list.SelectedItem.ToString();
+        var parts = selectedItem?.Split('-');
+        if (selectedItem != null && parts != null)
+        {
+            if (parts.Length >= 3 && int.TryParse(parts[2].Split(' ')[1], out var quantityOfItem))
+            {
+                allQuantity -= quantityOfItem;
+            }
+
+            list.Items.Remove(selectedItem);
+            stats.Content = $"Összes termék darabszáma: {allQuantity} db";
+        }
+        else
+        {
+            MessageBox.Show("Nincs kijelölve elem!");
+        }
+    }
+
+    private void save_Click(object sender, RoutedEventArgs e)
+    {
+        if (list.Items.Count == 0)
+        {
+            MessageBox.Show("Nincs semmi a listába!");
+            return;
+        }
+
+        if (File.Exists("raktar.txt"))
+        {
+            File.Delete("raktar.txt");
+        }
+
+        File.AppendAllLines("raktar.txt", list.Items.Cast<string>());
+    }
+
+    private void load_Click(object sender, RoutedEventArgs e)
+    {
+        if (!File.Exists("raktar.txt"))
+        {
+            MessageBox.Show("Nincs mentett lista.");
+            return;
+        }
+
+        list.Items.Clear();
+        File.ReadAllLines("raktar.txt").ToList().ForEach(x => list.Items.Add(x));
+    }
+}
